ad817e3ffd0d4a20fb6da7b51697757a508e2d07
PC <PC@DESKTOP-MA4RSJC>
2022-02-17 14:50:46 +0200

    creating basic structure

---
+++ of-chain/server/GoldPriceOracle/GoldPriceOracle.Node/Controllers/SetupController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoldPriceOracle.Node.Controllers
+{
+    [Route("api/setup")]
+    public class SetupController : ControllerBase
+    {
+        public SetupController()
+        {
+
+        }
+
+        [HttpGet("check")]
+        public IActionResult CheckNodeSetup()
+        {
+            return Ok();
+        }
+
+    }
+}
