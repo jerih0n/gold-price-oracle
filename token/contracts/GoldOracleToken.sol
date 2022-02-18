882026c7de7d551473d16b246cf2261e6de86276
PC <PC@DESKTOP-MA4RSJC>
2022-02-18 16:43:38 +0200

    update ERC20 token supply

--- token/contracts/GoldOracleToken.sol
+++ token/contracts/GoldOracleToken.sol
@@ -3,9 +3,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GoldOracleToken is ERC20 {
-    uint256 constant _initialSupply = 200000000;
+    uint256 constant _initialSupply = 200000000 * 10**18;
+    address private _owner;
 
     constructor() ERC20("GoldOracleToken", "GOT") {
         _mint(msg.sender, _initialSupply);
+        _owner = msg.sender;
+    }
+
+    function getOwnerAddress() public view returns (address) {
+        return _owner;
     }
 }
