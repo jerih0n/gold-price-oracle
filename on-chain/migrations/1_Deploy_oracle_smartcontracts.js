7453f8b314cfe45c7384987f4da9a4f04d3c311d
PC <PC@DESKTOP-MA4RSJC>
2022-02-26 18:30:17 +0200

    creating deployment sript for all contracts

--- on-chain/migrations/1_Deploy_oracle_smartcontracts.js
+++ on-chain/migrations/1_Deploy_oracle_smartcontracts.js
@@ -3,6 +3,7 @@ const Rounds = artifacts.require("Rounds");
 const Voters = artifacts.require("Votes");
 const GoldOracleToken = artifacts.require("GoldOracleToken");
 const Stakeholders = artifacts.require("Stakeholders");
+const GoldPriceAggregator = artifacts.require("GoldPriceAggregator");
 
 module.exports = async function (deployer) {
     deployer.then(async () => {
@@ -11,6 +12,10 @@ module.exports = async function (deployer) {
 
         await deployer.deploy(Rounds);
         await deployer.deploy(Voters);
+        await deployer.link(Rounds, GoldPriceResolver);
+        await deployer.link(Voters, GoldPriceResolver);
         await deployer.deploy(GoldPriceResolver);
+
+        await deployer.deploy(GoldPriceAggregator, GoldPriceResolver.address);
     });
 };
